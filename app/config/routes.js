5ffc02cc47863d750a94385260b05bf22659fcf9
Anton <anton.arboleda2gmail.com>
2016-10-28 17:40:29 -0700

    'deleted files to restart proj '# Please enter the commit message for your changes. Lines starting

--- app/config/routes.js
+++
@@ -1,9 +0,0 @@
-/*Configuration file*/
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { render } from 'react-dom';
-import {Router, Route, History } from 'react-router'
-
-var routes = (
-	<Route path='/' component={Main}>
-);
